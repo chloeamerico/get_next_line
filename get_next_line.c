e549e3f795560ff7c2fe56d0887d84fe70f04e5b
Chloe Americo <camerico@bess-f3r6s3.clusters.42paris.fr>
2024-12-09 18:15:54 +0100

    ca me soule c'est chaud

--- get_next_line.c
+++ get_next_line.c
@@ -6,9 +6,15 @@
 /*   By: camerico <camerico@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 15:06:18 by camerico          #+#    #+#             */
-/*   Updated: 2024/12/05 19:17:01 by camerico         ###   ########.fr       */
+/*   Updated: 2024/12/09 18:08:04 by camerico         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strjoin_free(char const *s1, char const *s2)
+char	*ft_strdup_free(const char *s);
+char	*ft_strchr(const char *s, int c);
 
 char	*get_next_line(int fd)
 {
@@ -16,33 +22,61 @@ char	*get_next_line(int fd)
 	static char	*stash;
 	char	*line;
 
+	if (fd < 0 || BUFFER_SIZE <= 0)
+			return (NULL);
 	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buffer)
 		return (NULL);
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		{
-			free(buffer);
-			return (NULL);
-		}
-	stash = 
-
+	line = manage_stash_and_line(&stash, buffer); // Gerer stash + extraire ligne
+	free(buffer);
+	return (line);
 }
 
-// pour lire le fichier et remplir le buffer avec n (BUFFER_SIZE) charact.
-static char	*read_to_buffer(int	fd, static char *stash, char *buffer)
+// pour lire le fichier associe a fd et remplir le buffer
+// avec n (BUFFER_SIZE) charact.
+char	*read_to_buffer(int fd, char *buffer)
 {
-	
+	ssize_t	b_read;
+
+	b_read = read(fd, buffer, BUFFER_SIZE);
+	if (b_read == -1)
+		return (NULL);
+	while (b_read > 0)
+		buffer[b_read] = '\0';
+	return (buffer);
 }
 
-// stash est une variable static
 // si stash vide, copier tout le buffer dans stash
 // si stash deja rempli, ajouter le coontenu de buffer dns stash
 // puis vider le buffer
-char	*manage_stash
-{
-	
-}
 
 // cherche le 1er \n ou \0. si trouve copie jusqu'a \n dans line 
 // en gardant le contenu restant ninon return NULL et continuer a lire
-char	*etract_line
+char	*manage_stash_and_line(char *stash, char *buffer)
+{
+	char *line;
+	char *tmp;
+	int	i;
+
+	i = 0;
+	if (stash =! NULL)
+		line = ft_strdup_free(stash);
+	while (buffer[i] || buffer[i] != '\n')
+		i++;
+	if (buffer[i] != '\n')
+	{
+		if (line == NULL)	
+		line = ft_strjoin_free(buffer)
+	}
+	else
+	{
+		line = ft_strchr(buffer, '\n');
+		stash = 
+	}
+	
+	else if (line == NULL)
+		line = ft_strjoi(buffer);
+	else
+		line = ft_strjoin_free
+	
+}
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,16 +6,84 @@
 /*   By: camerico <camerico@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 15:06:41 by camerico          #+#    #+#             */
-/*   Updated: 2024/12/04 16:44:21 by camerico         ###   ########.fr       */
+/*   Updated: 2024/12/09 16:15:03 by camerico         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int open (const char* path, int flags)
+#include "get_next_line.h"
+
+int	ft_strlen (const char *s)
 {
-	
+	int	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i])
+		i++;
+	return (i);
 }
 
-ssize_t	read(int fildes, void *buf, size_t nbyte)
+// si la nouvelle ligne est deja remplie , strjoinfree s1(line) et s2(buffer)
+// deja free dans la fonction
+char	*ft_strjoin_free(char const *s1, char const *s2)
 {
-	
+	int		i;
+	int		j;
+	int		k;
+	char	*s3;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	s3 = malloc(sizeof(char) * ((ft_strlen(s1) + ft_strlen(s2)) + 1));
+	if (!s3)
+		return (NULL);
+	while (s1[i] && (i == 0 || s1[i - 1] != '\n'))
+		s3[k++] = s1[i++];
+	while (s2[j])
+		s3[k++] = s2[j++];
+	s3[k] = '\0';
+	free(s1);
+	return (s3);
+}
+
+// retourne un pointeur vers une nouvelle string copie de *s
+// utilise quand la nvlle lign est vide, supplique buffer dans line
+// utilise quand stash est remplie(cntenu apres \n),duplique stash dans line
+// deja free dans la fonction
+char	*ft_strdup_free(const char *s)
+{
+	size_t	i;
+	char	*dest;
+
+	i = 0;
+	dest = malloc((ft_strlen(s) + 1) * sizeof(char));
+	if (dest == NULL)
+		return (NULL);
+	while (s[i])
+	{
+		dest[i] = s[i];
+		i++;
+	}
+	dest[i] = '\0';
+	free(s);
+	return (dest);
+}
+
+// recherche la premiere occurence du char c dans *s
+// retourne le reste de la string a partir du premier char c + 1 trouve (\n)
+// verifie s'il y a un \n dan le buffer
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i]) 
+	{
+		if (s[i] == (char)c)
+			return ((char *)(s + i + 1));
+		i++;
+	}
+	return (NULL);
 }
